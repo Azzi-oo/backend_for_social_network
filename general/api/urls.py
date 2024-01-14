327b8002789d5f1a2466aef0a038f0c2a47d9da9
Azzi-oo <yanberdin.az@yandex.ru>
2024-01-14 23:06:31 +0500

    add new factories

--- general/api/urls.py
+++ general/api/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import SimpleRouter
 
-from general.api.views import UserViewSet, PostViewSet, CommentViewSet, ReactionViewSet, ChatViewSet
+from general.api.views import UserViewSet, PostViewSet, CommentViewSet, ReactionViewSet, ChatViewSet, MessageViewSet 
 
 
 router = SimpleRouter()
@@ -10,5 +10,6 @@ router.register(r'posts', PostViewSet, basename="posts")
 router.register(r'comments', CommentViewSet, basename="comments")
 router.register(r'reaction', ReactionViewSet, basename="reaction")
 router.register(r'chats', ChatViewSet, basename="chats")
+router.register(r'messages', MessageViewSet, basename="messages")
 
 urlpatterns = router.urls
